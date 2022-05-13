585b4efca48cbf9737e458ca5a909a7d175e0f3f
Kaylan Prophet <kaylan.prophet@gmail.com>
2022-05-12 20:29:31 -0400

    added local storage

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -35,32 +35,57 @@ async function getWeather(citySearchVal) {
     }
 }
 
+//adds to local storage
+function addsToLocalStorage(city){
+    var cityList = JSON.parse(localStorage.getItem("cityList")) || []
+    if(cityList.includes(city)) return
+    cityList.push(city)
+    localStorage.setItem("cityList", JSON.stringify(cityList))
+}
+
+function addsButton(){
+var cityList = JSON.parse(localStorage.getItem("cityList")) || []
+var cityHistory = document.getElementById("searchHistory")
+cityHistory.innerHTML = ""
+cityList.forEach((city)=> {
+var cityButton = document.createElement("button")
+cityButton.textContent = city
+cityHistory.append(cityButton)
+})
+}
+
 //displays weather on page
 function displayWeather(currentWeatherData, forecastWeatherData) {
+addsToLocalStorage(currentWeatherData.name)
+addsButton()
 
     //placing js into the html
     mainDivEl.innerHTML = `
         <h2>${currentWeatherData.name}</h2>
-        <p>${currentWeatherData.weather.icon}
+        <img src="http://openweathermap.org/img/wn/${currentWeatherData.weather[0].icon}@2x.png"/>
         <p class="">Temp: ${currentWeatherData.main.temp} °f</p>
         <p>Humidity: ${currentWeatherData.main.humidity}</p>
         <p>Wind: ${currentWeatherData.wind.speed}</p>
         <br>
    
      `
+     console.log(forecastWeatherData)
+
+     var forecast = document.getElementById("forecast")
+     forecast.innerHTML = ""
      //for loop for date for 5 day forecast
      for (var i=0; i < 5; i++) {
+         var wrapperElement = document.createElement("div")
          var dateElement = document.createElement("p")
-         //TODO change the date from unix
-         dateElement.textContent = forecastWeatherData.daily[i].dt
-         document.getElementById("forecast").append(dateElement)
+         wrapperElement.classList.add("col")
+         var currentDate = new Date(parseInt(forecastWeatherData.daily[i].dt)*1000)
+         dateElement.textContent = currentDate.toLocaleDateString(undefined,{ weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })
+         var tempElement = document.createElement("p")
+         tempElement.textContent = forecastWeatherData.daily[i].temp.day
+         wrapperElement.append(dateElement, tempElement)
+         forecast.append(wrapperElement)
      }
-    //for loop for 5 day temp forecast
-    for (var i = 0; i < 5; i++) {
-        var tempElement = document.createElement("p")
-        tempElement.textContent = forecastWeatherData.daily[i].temp.day
-        document.getElementById("forecast").append(tempElement)
-    }
+
 
 
 
